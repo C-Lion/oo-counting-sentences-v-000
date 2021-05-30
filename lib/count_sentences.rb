50e195df5827c72cbdb0abce239423f513c76b6a
C-Lion Stepansky <esstepansky@gmail.com>
2021-05-30 10:22:07 +0000

    Automatically backed up by Learn

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -2,20 +2,19 @@ require 'pry'
 
 class String
 
-  def sentence?(string)
-    string.ends_with?(".")
-    
+  def sentence?
+    self.end_with?(".")
   end
 
   def question?
-
+    self.end_with?("?")
   end
 
   def exclamation?
-
+    self.end_with?("!")
   end
 
   def count_sentences
-
+    self.split(/\.|\?|\!/).delete_if {|w| w.size < 2}.size
   end
 end
